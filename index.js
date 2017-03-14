04be8322bf1d515c0ac3b083e6f354936331b056
Maxime ALARDO <rstoke@khronos>
2017-03-14 05:51:50 +0100

    add dConfig in index

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 module.exports = {
 
     Service: require('./factory/Service.js'),
-    Server: require('./factory/Server.js')
+    Server: require('./factory/Server.js'),
+
+    dConfig: require('./dConfig.js')
 
 };
