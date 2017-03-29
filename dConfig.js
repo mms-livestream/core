a1b8d84b37d8c1811e917390449be04bd0589330
Maxime ALARDO <rstoke@khronos>
2017-03-29 10:45:12 +0200

    temp fix for multi transcode

--- dConfig.js
+++ dConfig.js
@@ -9,12 +9,15 @@ let db = {
     NODE_INTELLIGENCE: {"service": {"host": "localhost", "port": 5003}, "server": {"host": "localhost", "port": 8083}},
     NODE_MPD_GENERATOR: {"service": {"host": "localhost", "port": 5004}, "server": {"host": "localhost", "port": 8084}},
     NODE_TRANSCODER: {"service": {"host": "localhost", "port": 5005}, "server": {"host": "localhost", "port": 8085}},
+    NODE_TRANSCODER1: {"service": {"host": "localhost", "port": 5005}, "server": {"host": "localhost", "port": 8085}},
+    NODE_TRANSCODER2: {"service": {"host": "localhost", "port": 5005}, "server": {"host": "localhost", "port": 8085}},
+    NODE_TRANSCODER3: {"service": {"host": "localhost", "port": 5005}, "server": {"host": "localhost", "port": 8085}},
     NODE_REPLICATOR: {"service": {"host": "localhost", "port": 5006}, "server": {"host": "localhost", "port": 8086}},
     NODE_DISTRIB: {"service": {"host": "localhost", "port": 5007}, "server": {"host": "localhost", "port": 8087}},
 };
 
 db.SERVICE_SCHEMA = {
-    "NODE_SESSION_MANAGER": [db.NODE_DB_CONTROLLER],//, db.NODE_MPD_GENERATOR],
+    "NODE_SESSION_MANAGER": [db.NODE_DB_CONTROLLER],//, db.NODE_MPD_GENERATOR], //TODO solve multi schema seneca client problem
     "NODE_METADATA_MANAGER": [db.NODE_DB_CONTROLLER],
     "NODE_DATABASE_CONTROLLER": [],
     "NODE_INTELLIGENCE": [db.NODE_DB_CONTROLLER, db.NODE_SESSION_MANAGER, db.NODE_TRANSCODER],
